2bf662b6b7fb3c21de7bddb6e58b74cb8a72076a
lukitas06 <lucasalegre13@gmail.com>
2022-11-16 17:14:52 -0300

    commit_

--- src/Library/Recipe.cs
+++ src/Library/Recipe.cs
@@ -9,12 +9,14 @@ using System.Collections.Generic;
 
 namespace Full_GRASP_And_SOLID
 {
-    public class Recipe : IRecipeContent // Modificado por DIP
+    public class Recipe : IRecipeContent,TimerClient // Modificado por DIP
     {
         // Cambiado por OCP
         private IList<BaseStep> steps = new List<BaseStep>();
 
         public Product FinalProduct { get; set; }
+
+        public  bool Cooked= false;
 
         // Agregado por Creator
         public void AddStep(Product input, double quantity, Equipment equipment, int time)
@@ -61,6 +63,24 @@ namespace Full_GRASP_And_SOLID
             }
 
             return result;
+        }
+        public int getCookTime()
+        {
+            int time=0;
+            foreach(BaseStep step in steps)
+            {
+                time+= step.Time;
+            }
+            return time;
+        }
+        public void Cook()
+        {
+            CountdownTimer timer = new CountdownTimer();
+            timer.Register(getCookTime(),this);
+        }
+        public void TimeOut()
+        {
+            this.Cooked=true;
         }
     }
 }
